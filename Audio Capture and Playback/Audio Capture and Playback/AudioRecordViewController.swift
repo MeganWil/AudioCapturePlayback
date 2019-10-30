652742593f091652e36feca57ad9ad09a731efb2
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-10-30 02:31:33 -0500

    added files

---
+++ Audio Capture and Playback/Audio Capture and Playback/AudioRecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AudioRecordViewController.swift
+//  Audio Capture and Playback
+//
+//  Created by Megan Wilson on 10/29/19.
+//  Copyright © 2019 Megan Wilson. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
